e8701fa4b4901a80e2a18679082ebf69a3305591
Denis <wolf-den@tut.by>
2022-05-31 18:50:27 +0300

    feat: add code for fs create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,33 @@
+import fs from 'fs';
+
 export const copy = async () => {
-    // Write your code here 
+  const path = 'files';
+  const pathCopy = 'files_copy';
+  const errorText = 'FS operation failed';
+
+  fs.access(path, (errNoException) => {
+    if (!errNoException) {
+      fs.access(pathCopy, (errNoException) => {
+        if (errNoException) {
+          fs.mkdir(pathCopy, (err) => {
+            if (err) throw err;
+            fs.readdir(path, (err, files) => {
+              if (err) throw err;
+              files.forEach(file => {
+                fs.copyFile(`${path}/${file}`, `${pathCopy}/${file}`, err => {
+                  if (err) throw err;
+                });
+              });
+            })
+          });
+        } else {
+          throw new Error(errorText);
+        }
+      });
+    } else {
+      throw new Error(errorText);
+    }
+  });
 };
+
+copy();

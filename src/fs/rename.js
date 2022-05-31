8463f02fb11a7891c0080e85a0268144e1fe2c23
Denis <wolf-den@tut.by>
2022-05-31 19:04:07 +0300

    feat: add code for fs rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,23 @@
+import fs from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+  const path = 'files/wrongFilename.txt';
+  const pathRename = 'files/properFilename.md';
+  const errorText = 'FS operation failed';
+
+  fs.access(path, (errNoException) => {
+    if (!errNoException) {
+      fs.access(pathRename, (errNoException) => {
+        if (errNoException) {
+          fs.rename(path, pathRename, err => {
+            if (err) throw err;
+          });
+        } else {
+          throw new Error(errorText);
+        }
+      });
+    } else {
+      throw new Error(errorText);
+    }
+  });
 };

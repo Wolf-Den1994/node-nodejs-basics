e7ce040408fef114252ce11875b1dc6f6d0a850f
Denis <wolf-den@tut.by>
2022-05-31 19:43:52 +0300

    feat: add code for fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
+
 export const remove = async () => {
-    // Write your code here 
+  const path = 'files/fileToRemove.txt';
+  const errorText = 'FS operation failed';
+
+  fs.access(path, (errNoException) => {
+    if (!errNoException) {
+      fs.unlink(path, err => {
+        if (err) throw err;
+        console.log('File has been deleted');
+      });
+    } else {
+      throw new Error(errorText);
+    }
+  });
 };
+
+remove();

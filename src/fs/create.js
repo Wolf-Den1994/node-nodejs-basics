aee27325edf0b33864f5334d0081f447ad340045
Denis <wolf-den@tut.by>
2022-05-31 18:49:06 +0300

    feat: add code for fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import fs from 'fs';
+
 export const create = async () => {
-    // Write your code here 
+  const path = 'files/fresh.txt';
+  const data = 'I am fresh and young';
+  const errorText = 'FS operation failed';
+
+  fs.access(path, (errNoException) => {
+    if (errNoException) {
+      fs.open(path, 'w', (err) => {
+        if (err) throw err;
+      });
+      fs.appendFile(path, data, (err) => {
+        if (err) throw err;
+      });
+    } else {
+      throw new Error(errorText);
+    }
+  });
 };

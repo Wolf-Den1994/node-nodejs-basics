bc8164bf0e606366ceba8fd72d9bcb80fe8e24ca
Denis <wolf-den@tut.by>
2022-06-01 23:02:58 +0300

    feat: add function env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
+import { env } from 'node:process';
+
 export const parseEnv = () => {
-    // Write your code here 
+  let result = '';
+  const query = 'RSS_';
+  const entries = Object.entries(env);
+  const data = entries.filter((entry) => entry[0].includes(query));
+  data.forEach((item, index) => {
+    if (data.length - 1 === index) result += `${item[0]}=${item[1]}`;
+    else result += `${item[0]}=${item[1]}; `;
+  });
+  console.log(result);
 };
+
+parseEnv();

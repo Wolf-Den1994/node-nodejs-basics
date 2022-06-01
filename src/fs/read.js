3d3b291847e997f3114507101d26f769090ed915
Denis <wolf-den@tut.by>
2022-06-01 21:07:36 +0300

    feat: add function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,24 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { errorText } from '../utils/constants.js';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path = `${__dirname}/files/fileToRead.txt`;
+
+  try {
+    await fs.access(path);
+    throw new Error(errorText);
+  } catch (error) {
+    if (error.message === errorText) {
+      const promise = await fs.readFile(path, { encoding: 'utf8' });
+      console.log(promise);
+    } else {
+      throw new Error(errorText);
+    }
+  }
 };
+
+read();

f9776e3c7db2a2c564c8e98af8cd86e83badc4fa
Denis <wolf-den@tut.by>
2022-06-03 19:31:29 +0300

    feat: add caclHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,21 @@
+import crypto from 'crypto';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+  try {
+    const text = await fs.readFile(path, { encoding: 'utf8' });
+    const result = crypto.createHash('sha256').update(text).digest('hex');
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
+
+calculateHash();
